2c313ef64b99a301980efddc662771a6915bbf14
shivam raj <shivamkrishna9142@gmail.com>
2026-07-01 21:35:17 +0530

    conversion of datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountid=133333
+let accountemail="shivaraj@gmail.com"
+var accountpassword="344554"
+accountcity="patna"
+let accountstate;
+accuntid=2
+
+accountemail="jhsgvhsjv@gmai.com"
+accountpassword="987e8"
+accountcity="bengluru"
+console.log(accountid);
+
+/*prefer not to use var because of issue in block scope and function scope*/
+
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) we are using node.js,not browser
+
+let name="shivam raj"
+let age=18
+let isloggedin=false
+let state;
+console.log(typeof undefined);//undefined
+console.log(typeof null)//objedt
