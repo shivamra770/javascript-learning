e2cb36761c8e9b5d0ea3261c7c6e24d668f9b448
shivam raj <shivamkrishna9142@gmail.com>
2026-07-02 06:46:24 +0530

    datatype summary

---
+++ 01_basics/datatypes_sumamry.js
@@ -0,0 +1,32 @@
+//primitive datatype
+
+//7 types:string,number.boolean,null,undefined,symbol,bigint
+
+const score=100
+const scorevalue=100.3
+
+const isloggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+// const bigNumber=87362746234n
+
+
+//reference type (non primitive)
+
+//arrays,object,function
+
+const heros=["shaktiman","Avengers","Ben 10"];
+let myobj={
+    name:"shivam",
+    age:21,
+}
+
+const myfunction=function(){
+    console.log("hello world");
+}
+console.log(typeof anotherid);
